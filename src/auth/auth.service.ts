7a938aca98c13dee6c76cd159960ad4c8499601c
Ehsan Moallaee <ehsanm78@gmail.com>
2023-11-04 05:14:18 +0330

    Sign up and sign in methods done in auth service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from "@nestjs/common";
+import { ForbiddenException, Injectable } from "@nestjs/common";
 import * as argon from 'argon2';
 import { PrismaService } from "src/prisma/prisma.service";
 import { AuthDto } from "./dto";
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
 
 @Injectable()
 export class AuthService {
@@ -9,18 +10,36 @@ export class AuthService {
     constructor(private readonly prismaService: PrismaService) {}
 
     async signup(dto: AuthDto) {
-        const hashedPassword = await argon.hash(dto.password)
-        const user = await this.prismaService.user.create({
-            data: {
-                email: dto.email,
-                password: hashedPassword
+        try {
+            const hashedPassword = await argon.hash(dto.password)
+            const user = await this.prismaService.user.create({
+                data: {
+                    email: dto.email,
+                    password: hashedPassword
+                }
+            });
+            delete user.password;
+            return user;
+        } catch (err) {
+            if(err instanceof PrismaClientKnownRequestError) {
+                if(err.code === 'P2002') {
+                    throw new ForbiddenException('Credentials taken');
+                }
             }
-        });
-        delete user.password;
-        return user;
+            throw err;
+        }        
     }
     
-    signin() {
-        return { msg: 'I have signed in'}
+    async signin(dto: AuthDto) {
+        const user = await this.prismaService.user.findUnique({
+            where: { email: dto.email }
+        })
+        if(!user) throw new ForbiddenException('Credentials incorrect');
+
+        const passwordMatch = await argon.verify(user.password, dto.password);
+        if(!passwordMatch) throw new ForbiddenException('Credentials incorrect');
+
+        delete user.password;
+        return user;
     }
 }
