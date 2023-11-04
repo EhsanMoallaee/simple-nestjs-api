bf16b755fd79b308d1b77d57d81179998c12609a
Ehsan Moallaee <ehsanm78@gmail.com>
2023-11-04 04:44:03 +0330

    Class-validator,class-transformer and argon2 packages are installed,authDto created and signup controller and service improved

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,13 +1,23 @@
 import { Injectable } from "@nestjs/common";
+import * as argon from 'argon2';
 import { PrismaService } from "src/prisma/prisma.service";
+import { AuthDto } from "./dto";
 
 @Injectable()
 export class AuthService {
 
     constructor(private readonly prismaService: PrismaService) {}
 
-    signup() {
-        return { msg: 'I have signed up'}
+    async signup(dto: AuthDto) {
+        const hashedPassword = await argon.hash(dto.password)
+        const user = await this.prismaService.user.create({
+            data: {
+                email: dto.email,
+                password: hashedPassword
+            }
+        });
+        delete user.password;
+        return user;
     }
     
     signin() {
