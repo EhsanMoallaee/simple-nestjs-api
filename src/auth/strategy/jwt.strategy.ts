ebc19db5d07e8fa525c01d606cda9e5cbce83819
Ehsan Moallaee <ehsanm78@gmail.com>
2023-11-07 21:32:04 +0330

    Jwt.guard, get-user.decorator and get user profile route done

--- src/auth/strategy/jwt.strategy.ts
+++ src/auth/strategy/jwt.strategy.ts
@@ -2,13 +2,27 @@ import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { PassportStrategy } from "@nestjs/passport";
 import { ExtractJwt, Strategy } from "passport-jwt";
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class JwtStrategy extends PassportStrategy(Strategy) {
-    constructor(private readonly configService: ConfigService) {
+export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
+    constructor(
+        private readonly configService: ConfigService,
+        private readonly prismaService: PrismaService    
+    ) {
         super({
             jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
             secretOrKey: configService.get('JWT_SECRET')
         })
+    }
+
+    async validate(payload: {sub: number, email: string}) {
+        const user = await this.prismaService.user.findUnique({
+            where: {
+                id: payload.sub
+            }
+        });
+        delete user.password;
+        return user;
     }
 }
